e4d05c1434e59ef1ae80d26770a86adbf2991022
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2022-02-24 11:31:28 -0500

    Update True or False Checker.js

--- True or False Checker.js
+++ True or False Checker.js
@@ -10,17 +10,21 @@
 	if(truestat())
 	{ 
   		console.log('The Statement "true" Was Found.');
+		alert('The Statement "true" Was Found.');
    }
 	else
 	{
   		console.log('No "true" Statements Where Found.');
+		alert('No "true" Statements Where Found.');
    }
 
 	if(falsestat())
 	{ 
   		console.log('The Statement "false" Was Found');
+		alert('The Statement "false" Was Found.');
    }
 	else
 	{
   		console.log('No "false" Statements Where Found.');
+		alert('No "false" Statements Where Found');
    }
