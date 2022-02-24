b8027b5defcb8da91bdc51eb79fad41443333211
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2022-02-24 11:28:57 -0500

    Add files via upload

---
+++ True or False Checker.js
@@ -0,0 +1,26 @@
+   function truestat()
+	{
+   return true;
+   }
+   function falsestat()
+	{
+   return false;
+   }
+
+	if(truestat())
+	{ 
+  		console.log('The Statement "true" Was Found.');
+   }
+	else
+	{
+  		console.log('No "true" Statements Where Found.');
+   }
+
+	if(falsestat())
+	{ 
+  		console.log('The Statement "false" Was Found');
+   }
+	else
+	{
+  		console.log('No "false" Statements Where Found.');
+   }
