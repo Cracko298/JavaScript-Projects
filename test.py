9429267ec469aa2f6b48e4dd3c9153bbd206a66d
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2022-03-11 09:21:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,30 @@
 Link = https://pypi.org/project/auto-py-to-exe/ #Link to convert .py to .exe
+Link = https://realpython.com/pysimplegui-python/ #Link to Python Simple Gui
 
-
-#Actual Code
 import sys
+import os
+import PySimpleGUI as sg #Make sure to download this: pip install pysimplegui
 
-data_in_byte = b'\x0F\x27'
-Data0 = "Data0"
+#GUI Text Code
+layout = [[sg.Text("9999 Health")], [sg.Button("Create")]]
 
-#Repl.it corrupts data files. Try on actual machine at later date
-with open(Data0, 'rb+') as binary_file:
- binary_file.seek(33)
- binary_file.write(data_in_byte)
+#GUI Window Code
+window = sg.Window(title="Ice Station Z Save Editor", layout=[[]], margins=(640, 960)).read()
+
+#GUI Loop Code
+while True:
+    event, values = window.read()
+    if event == ""Create" or event == sg.WIN_CLOSED:
+        #Closes window if application is closed or the 9999 Health Button is pressed (Change)
+        break #Exits Application
+
+window.close() #Closes Window
+
+
+#Edit Data in Bytes Code.
+data_in_byte = b'\x0F\x27' #Bytes to write to file
+Data0 = "Data0" #Name of file
+
+with open(Data0, 'rb+') as binary_file: #open file as binary
+ binary_file.seek(33) #Move Editing to Offset 33
+ binary_file.write(data_in_byte) #Writes the bytes from "data_in_byte"
